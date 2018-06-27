299b3576838ac3e544b247b98237abfb136c01ae
maceurt <maxballard333@gmail.com>
2018-06-27 11:01:33 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var slide_position = 2;
+
+function showSlide() {
+	console.log(slide_position);
+	$("#slideshow img").hide();
+	$("#slideshow img:nth-child(" + slide_position + ")").show();
+}
+
+function moveSlidePosition(move) {
+	slide_position += move;
+	if (slide_position < 1) {
+		slide_position = $("#slideshow img").length + slide_position;
+	}
+	else {
+		slide_position = Math.max(1, slide_position % ($("#slideshow img").length + 1));
+	}
+	showSlide();
+}
+
+$(document).ready(function() {
+	showSlide();
+	
+	
+});
